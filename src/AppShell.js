39a2c74ccea6a1198642f5108448082b0b63a786
jacksondev <jacksondev8@gmail.com>
2017-10-10 00:47:25 -0300

    primeiro commit

---
+++ src/AppShell.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { Switch, Route, Link } from 'react-router-dom';
+import View1 from './View1';
+import View2 from './View2';
+import View3 from './View3';
+
+class AppShell extends Component {
+  render() {
+    return (
+     <div>
+       <header>
+          <Link to="/page1"> Page 1 </Link>
+          <Link to="/page2"> Page 2 </Link>
+          <Link to="/page3"> Page 3 </Link> 
+       </header> 
+       <main>
+           <Switch>
+             <Route exact path="/page1" component={View1}/>
+             <Route path="/page2" component={View2}/>
+             <Route path="/page3" component={View3}/>
+           </Switch>  
+       </main>
+       <footer>
+         made with love
+       </footer>  
+     </div>
+    );
+  }
+}
+
+export default AppShell;
